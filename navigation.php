c764aad230891fe5aac620137aa429744ac4b58c
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-02-29 12:48:21 +0100

    eerste wijzigingen

---
+++ navigation.php
@@ -0,0 +1,21 @@
+<div class="col-12" id="navbar">
+  <nav class="navbar navbar-expand-lg">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="./index.php?content=home">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</div>
---
+++ pages/home.php
@@ -0,0 +1,11 @@
+
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <img src="" alt="">
+        </div>
+        <div class="col-6"  id="home">
+            <h1>welkomstwoord</h1>
+        </div>
+    </div>
+<div>
