a53d00e3eb467e0ab485becf8abf765558f7b00e
Aron van de Ridder <aronridder@hotmail.nl>
2020-03-02 14:01:37 +0100

    NAVBAR WERKT BICHES

--- navigation.php
+++ navigation.php
@@ -13,7 +13,7 @@
           <a class="nav-link" id="nav-item" href="#">Features</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" id="nav-item" href="#">Pricing</a>
         </li>
       </ul>
     </div>
