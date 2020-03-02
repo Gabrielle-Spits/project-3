a49ea4ea1253a2d69e81847121f164b92b94053e
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-03-02 15:14:39 +0100

    spelen

--- pages/home.php
+++ pages/home.php
@@ -1,22 +1,25 @@
 <div class="container">
     <div class="row">
         <div class="col-6">
-            <img src="./img/home.PNG" alt="" width="550px">
+            <img src="./img/home.PNG" id="img1" alt="">
         </div>
         <div class="col-6">
             <h1 id="home-titels">Welkomstwoord</h1>
             <div id="inhoud">
-            <p>Beste allemaal,</p>
-            <p>
-                Welkom op deze website. Hier vind je informatie over adhd.
-                Krijg je tips over hoe je met mensen met adhd moet omgaan.
-                En voor de adhd’ers zelf hoe je er mee kunt omgaan en hoe je anderen op de hoogte kunt brengen als er
-                iets is.
-                Ook kun je een leerzame game spelen en je aanmelden voor nog meer informatie.
-            </p>
-            <p>Hopelijk helpt deze site je! </p>
+                <p>Beste allemaal,</p>
+                <p>
+                    Welkom op deze website. Hier vind je informatie over adhd.
+                    Krijg je tips over hoe je met mensen met adhd moet omgaan.
+                    En voor de adhd’ers zelf hoe je er mee kunt omgaan en hoe je anderen op de hoogte kunt brengen als
+                    er
+                    iets is.
+                    Ook kun je een leerzame game spelen en je aanmelden voor nog meer informatie.
+                </p>
+                <p>Hopelijk helpt deze site je! </p>
             </div>
-        <!-- hier onder kun je naar de pagina's -->
+            <!-- hier onder kun je naar de pagina's -->
+            <a href="./index.php?content=informatie" class="btn btn-secondary" id="button">Informatie hier!</a>
+            
         </div>
     </div>
 </div>
