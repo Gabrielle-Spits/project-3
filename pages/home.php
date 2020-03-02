581211f467f71d666544b5ba0816546f6e9e4c99
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-03-02 12:04:03 +0100

    dag 2

--- pages/home.php
+++ pages/home.php
@@ -1,4 +1,3 @@
-
 <div class="container">
     <div class="row">
         <div class="col-6">
@@ -6,7 +5,17 @@
         </div>
         <div class="col-6">
             <h1 id="home-titels">Welkomstwoord</h1>
-            <p>Beste allemaal welkom op deze website hier vind je dingen over adhd en als je adhd hebt vind je hier tips</p>
+            <div id="inhoud">
+            <p>Beste allemaal,</p>
+            <p>
+                Welkom op deze website. Hier vind je informatie over adhd.
+                Krijg je tips over hoe je met mensen met adhd moet omgaan.
+                En voor de adhd’ers zelf hoe je er mee kunt omgaan en hoe je anderen op de hoogte kunt brengen als er
+                iets is.
+                Ook kun je een leerzame game spelen en je aanmelden voor nog meer informatie.
+            </p>
+            <p>Hopelijk helpt deze site je! </p>
+            </div>
         </div>
     </div>
-<div>
+    <div>
