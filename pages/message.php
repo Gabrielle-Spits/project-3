e641aff1fa81572178f008b84b4c862ba3b69a9a
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-03-26 15:44:12 +0100

    activatiemail sturen

--- pages/message.php
+++ pages/message.php
@@ -13,15 +13,20 @@ switch($_GET["alert"]){
     header("Refresh: 3; url=./index.php?content=registreer");
     break;
     case 'registreren-error':
-        echo '<div class="alert alert-warning w-50 mx-auto mt-5" role="alert">
+        echo '<div class="alert alert-danger w-50 mx-auto mt-5" role="alert">
                 er is iets fout gegaan probeer het opnieuw
             </div>';
         header("Refresh: 3; url=./index.php?content=registreer");
     break;
-    
+    case 'registreren-goed':
+        echo '<div class="alert alert-success w-50 mx-auto mt-5" role="alert">
+        het is gelukt u krijgt een activatie-mailtje.
+    </div>';
+    header("Refresh: 3; url=./index.php?content=login");
+    break;
     default: 
         header("Location: ./index.php?content=home");
     break;
 
 }
-?> 
+?>
--- pages/registreren-script.php
+++ pages/registreren-script.php
@@ -20,6 +20,35 @@ else{
 
    }else{
         //email toevoegen aan de tabel
+        $password = "geheim";
+        $password_hash = password_hash($password,PASSWORD_BCRYPT);
+
+        $sql = "INSERT INTO `registreren_blok3`
+         (`id`, 
+         `email`,
+         `wachtwoord`, 
+         `rollen`) 
+         VALUES 
+         (NULL, 
+         '$email', 
+         '$password_hash', 
+         'customer')";
+
+     if (mysqli_query($conn,$sql)){
+        //mail versturen
+        $to = $email;
+        $subject = "activatielink voor uw acount op http://www.project-3.org/";
+        $message ="dit is een test";
+        $headers ="From: admin@ ";
+
+        mail($to,$subject,$message,$headers);
+
+        header("Location: ./index.php?content=message&alert=registreren-goed");
+     }else{
+         //error melding
+         header("Location: ./index.php?content=message&alert=registreren-error");
+     };
+
    };
 }
  
