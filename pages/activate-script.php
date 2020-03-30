67916da943a38f21b79c0abd19ed6da23a0e2702
Fabién Kouwenberg <fabienko266@gmail.com>
2020-03-30 15:20:41 +0200

    test2

--- pages/activate-script.php
+++ pages/activate-script.php
@@ -7,7 +7,7 @@ $pwh = sanitize($_POST["pwh"]);
 $password = sanitize($_POST["password"]);
 $passwordcheck = sanitize($_POST["passwordcheck"]);
 
-if(empty($_POST["password"]) || (empty($_POST["passwordcheck"]))){
+if(empty($_POST["password"]) || empty($_POST["passwordcheck"])){
     header("Location: ./index.php?content=message&alert=geen-wachtwoord&id=$id&pwh=$pwh");
 } elseif(strcmp($password,$passwordcheck)) {
     header("Location: ./index.php?content=message&alert=geen-match-wachtwoord&id=$id&pwh=$pwh");
