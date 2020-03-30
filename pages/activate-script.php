983c90beac202d9d9a886a7a70d22a9aba674b3a
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-03-30 15:12:01 +0200

    fout ergens

--- pages/activate-script.php
+++ pages/activate-script.php
@@ -11,16 +11,8 @@ if(empty($_POST["password"]) || (empty($_POST["passwordcheck"]))){
     header("Location: ./index.php?content=message&alert=geen-wachtwoord&id=$id&pwh=$pwh");
 } elseif(strcmp($password,$passwordcheck)) {
     header("Location: ./index.php?content=message&alert=geen-match-wachtwoord&id=$id&pwh=$pwh");
-}else{
-  $sql="SELECT * FROM `registreren_blok3` Where `id` = $id AND `wachtwoord` = '$pwh'";
+} else{
+    
 
-  $result= mysqli_query($conn,$sql);
-
-  if(mysqli_num_rows($result)){
-      //updaten
-  }else{
-      
-  }
-
-  }
+}
 ?>
