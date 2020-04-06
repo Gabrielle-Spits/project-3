924a2b1adbf3997c2c0809886f62e04688deab3f
Fabién Kouwenberg <fabienko266@gmail.com>
2020-04-06 15:03:52 +0200

    tips_2

--- pages/tips.php
+++ pages/tips.php
@@ -1,44 +1,31 @@
 <div class="row">
-    <div class="col-3">
-    <h2>ADHD bij je kind</h2>
-    <p>Bied structuur en regelmaat. Denk aan een vaste dagindeling, of op vaste tijdstippen eten.</p>
-    <p>Zorg voor een opgeruimde kamer en bureau van je kind. Dit geeft minder prikkels.</p>
+    <div class="col-6">
+    <h2>Heb jij ADHD?</h2>
+    <p>Zorg voor een opgeruimde kamer en bureau. Dit geeft minder prikkels.</p>
+    <p>Zorg voor structuur en regelmaat. Denk aan een vaste dagindeling, of op vaste tijdstippen eten.</p>
+    <p>Voorkom afleiding. Zet de pop-ups van binnenkomende mails uit. Lees en beantwoord de mails op een vast moment op de dag.</p>
+    <p>Werken met deadlines zijn een goede motivator en stimulans voor je. Ook dit helpt om je te concentreren. Dus ook grote werkzaamheden zijn geschikt voor je, als je die tenminste opdeelt in deelwerkzaamheden met ieder een eigen deadline.</p>
+    <p>Onderbreek je werk door bv koffie te halen. Vraag aan je docent of je even een rondje mag lopen. Zo kun je je energie kwijt.</p>
+    <p>Het kan helpen om 's avonds, voor het slapen gaan, de belangrijkste zaken die op dat moment door je hoofd heen schieten op te schrijven. Dat geeft rust en doordoor ook een betere nachtrust.</p>    
     <p>Plan activiteiten en werkzaamheden. Splits deze in kleine delen. Dat geeft overzicht en daarmee rust.</p>
-    <p>Spreek van te voren stapsgewijs iets door; een verjaardag, familiebezoek etc. Maak de nieuwe situatie voorspelbaar.</p>
-    <p>Wees concreet. Vertel iets in korte zinnen, op duidelijke toon en kijk je kind in de ogen. Zo heb je contact.</p>
-    <p>Maak heldere afspraken. Herhaal wat er is afgesproken en vraag na of je kind het begrepen heeft of dat er nog vragen zijn.</p>
-    <p>Laat je kind regelmatig sporten. Geef je kind de gelegenheid om zijn/haar energie kwijt te raken en zicht uit te leven.</p>
+    <p>Ga regelmatig sporten. Geef jezelf de gelegenheid om je energie kwijt te raken en zicht uit te leven.</p>
+    <p>Maak een planning, eventueel met je leidinggevende, docenten, ouders of naaste collega’s.</p>
+    <p>Blijf met elkaar praten, verval niet in verwijten. Een gouden regel die voor elke relatie/vriendschap geldt. Erger je je aan iets, zeg dat dan en zoek ook naar een oplossing. ‘Ik vind het niet leuk dat je telkens te laat komt. Ik zat op je te wachten met eten. Is het anders een idee dat je een melding/alarmering maakt in je telefoon als reminder?'</p>
     </div>
 
-    <div class="col-3">
-    <h2>ADHD en volwassenen</h2>
+    <div class="col-6">
+    <h2>Ken je iemand met ADHD?</h2>
     <p>ADHD is geen excuus. Net als iedereen is ook iemand met ADHD verantwoordelijk voor zijn eigen daden en gedrag. Als iemand met ADHD afspraken niet nakomt, of als hij/zij dingen doet of zegt die je niet leuk vindt, zeg dit dat ook.</p>
+    <p>Bij mensen met ADHD is het vaak zwart/wit. Geen tint grijs ertussen. Wat de ander zegt is hoogstwaarschijnlijk niet zo bedoeld. Met die relativering in het achterhoofd is een (boze) opmerking al meteen een stuk minder erg. Wat helpt is dat je laat blijken dat je de ander gehoord hebt. ‘Ik begrijp dat je boos bent …’ of vat het samen ‘Als ik je goed begrijp vind jij dat …’. Aan de andere kant moet de een ook opletten wat hij/zij zegt en hoe iets gezegd wordt. De ander met ADHD kan dit heel anders opvatten.</p>
     <p>Iemand met ADHD is geen patient, behandel diegene dan ook niet op die manier. Je bent ook niet de hulpverlener. De ander heeft ook zijn eigen verantwoordelijkheden.</p>
     <p>Je kunt de ander wel behulpzaam zijn. Mocht de ander vaak afspraken vergeten, probeer een manier te vinden om de ander eraan te herinneren. De afspraak en een herinnering in de telefoon zetten, een app sturen zijn suggesties die je kunt proberen. Informeer een eventuele partner. Heeft je vriend(in) thuis een planbord, zorg er dan voor dat de ander de afspraak erop schrijft.</p>
     <p>Gestructureerdheid is waarschijnlijk geen kwaliteit van de ander met ADHD. Om last minute onaangename verrassingen te voorkomen kun je in sommige gevallen zelf zaken regelen en bewaren. Samen een weekje weg in het buitenland? Jij bewaart de reservering en ticket, controleert samen of het paspoort nog geldig is. Het liefst bewaar je ook het paspoort van de ander. Het zou jammer zijn als je de vlucht mist omdat de ander het paspoort of ticket vergeten is.</p>
     <p>Laat conventionele verwachtingen los. Het gaat soms anders dan je zou verwachten. Laat je verrassen en accepteer dat het soms gaat zoals het gaat. Laat je dus verrassen.</p>
     <p>En net zoals voor iedere vriendschap of relatie geldt: blijf communiceren! Het voorkomt misverstanden en teleurstellingen.</p>
-    </div>
-
-    <div class="col-3">
-    <h2>ADHD en werk</h2>
-    <p>Werken met deadlines zijn een goede motivator en stimulans voor je. Ook dit helpt om je te concentreren. Dus ook grote werkzaamheden zijn geschikt voor je, als je die tenminste opdeelt in deelwerkzaamheden met ieder een eigen deadline.</p>
-    <p>Voorkom afleiding. Zet de pop-ups van binnenkomende mails uit. Lees en beantwoord de mails op een vast moment op de dag. Zorg voor een opgeruimd bureau.</p>
-    <p>Maak een planning, eventueel met je leidinggevende of naaste collega’s.</p>
-    <p>Onderbreek je werk door bv koffie te halen. Ga lunchwandelen. Zo kun je je energie kwijt.</p>
-    </div>
-
-    <div class="col-3">
-    <h2>ADHD en relaties</h2>
-    <p>Elkaar beter leren kennen legt de fundering onder jullie relatie. Voor de een betekent dit je echt verdiepen in ADHD en in je partner met ADHD. Voor de ander betekent dit uitzoeken wat verwachtingen zijn, hoe je daar aan kunt voldoen door rekening met elkaar te houden.</p>
-    <p>Blijf met elkaar praten, verval niet in verwijten. Een gouden regel die voor elke relatie geldt. Erger je je aan iets, zeg dat dan en zoek ook naar een oplossing. ‘Ik vind het niet leuk dat je telkens te laat komt. Ik zat op je te wachten met eten. Is het anders een idee dat je een melding/alarmering maakt in je telefoon als reminder?'</p>
-    <p>Spontaan is leuk, maar het kan de ander ook overvallen. Als je elkaar langer kent, dan kun je de ander ook beter peilen. Dan herken je de signalen dat de ander eigenlijk even met rust gelaten wil worden. Zeg dan dat je iets wilt bespreken en wanneer je dat dan concreet wilt doen. Degene met ADHD kan zelf ook aangeven ‘even met rust gelaten te worden.’</p>
-    <p>Reserveer een (of meerdere) vaste avonden/momenten voor elkaar. Niet alleen geeft dat structuur voor degene met ADHD, maar het zorgt ook voor quality time voor jullie beiden. Tijd voor een goed gesprek, samen iets ondernemen, de afgelopen week eens doornemen en wat staat er volgende week op de agenda, of een mooi moment voor jullie liefde voor elkaar te tonen. Echt samen, geen afleiding in de vorm van een televisie of telefoon die bliept.</p>
-    <p>Een hulpmiddel kan een planbord zijn waarop goed zichbaar staat welke afspraken er die week zijn, welke klusjes gedaan moeten worden, welke boodschappen gehaald moeten worden, wat er welke dag gegeten gaat worden. Dit geeft overzicht en structuur.</p>
-    <p>Bij mensen met ADHD is het vaak zwart/wit. Geen tint grijs ertussen. Wat de ander zegt is hoogstwaarschijnlijk niet zo bedoeld. Met die relativering in het achterhoofd is een (boze) opmerking al meteen een stuk minder erg. Wat helpt is dat je laat blijken dat je de ander gehoord hebt. ‘Ik begrijp dat je boos bent …’ of vat het samen ‘Als ik je goed begrijp vind jij dat …’. Aan de andere kant moet de een ook opletten wat hij/zij zegt en hoe iets gezegd wordt. De ander met ADHD kan dit heel anders opvatten.</p>
     <p>Denk niet voor de ander. Je bent geen gedachtelezer. Stel actief vragen en luister. Probeer de ander niet te onderbreken. Houd oogcontact. Dit helpt dat de ander niet afdwaalt.</p>
-    <p>Het kan helpen om 's avonds, voor het slapen gaan, de belangrijkste zaken die op dat moment door het hoofd van degene met ADHD schieten op te schrijven. Dat geeft rust en doordoor ook een betere nachtrust.</p>
-    <p>Relativeer en probeer de humor van de onvermijdelijke misverstanden en miscommunicaties in te zien. Lachen lucht op waardoor de spanning ook weer verdwijnt.</p>
-    <p>Probeer een manier te vinden om samen als team te werken en op die manier ook aan ergernissen.</p>
+    <p>Een hulpmiddel kan een planbord zijn waarop goed zichbaar staat welke afspraken er die week zijn, welke klusjes gedaan moeten worden, welke boodschappen gehaald moeten worden, wat er welke dag gegeten gaat worden. Dit geeft overzicht en structuur.</p>
+    <p>Spontaan is leuk, maar het kan de ander ook overvallen. Als je elkaar langer kent, dan kun je de ander ook beter peilen. Dan herken je de signalen dat de ander eigenlijk even met rust gelaten wil worden. Zeg dan dat je iets wilt bespreken en wanneer je dat dan concreet wilt doen. Degene met ADHD kan zelf ook aangeven ‘even met rust gelaten te worden.’</p>
+    <p>Wees concreet. Vertel iets in korte zinnen, op duidelijke toon en kijk de ander in de ogen. Zo heb je contact.</p>
+    <p>Maak heldere afspraken. Herhaal wat er is afgesproken en vraag na of de ander het begrepen heeft of dat er nog vragen zijn.</p>
     </div>
 </div>
