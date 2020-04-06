f124fd99bd6ef5a8b37382d85133233d2b7887c1
Aron van de Ridder <aronridder@hotmail.nl>
2020-04-06 10:29:15 +0200

    2.0

--- pages/informatie.php
+++ pages/informatie.php
@@ -1,7 +1,32 @@
 <div class="row">
     <div class="col-4">
-        <h1 id="home-titels">Wat is ADHD?</h1>
+        <h1 id="home-titels">Wat betekent ADHD?</h1>
         <p>ADHD in een afkorting van  Attention Deficit Hyperactivity Disorder oftwel in het Nederlands ook wel bekend als
         aandachtstekortstoornis met hyper activiteit</p>
+    </div>
+    <div class="col-4">
+        <h1 id="home-titels">Hoe beleef je ADHD?</h1>
+        <p>ADHD is een stoornis waarmee je altijd heel veel gedachtes in je hoofd hebt. Als voorbeeld waar mensen zonder ADHD 10 gedachtes hebben per minuut hebben wij 
+            100 dingen die tegelijk in hun hoofd.</p>
+    </div>
+            <div class="row">
+            <div class="col-1">
+            </div>
+            <div class="col-6">
+        <h1 id="home-titels">Vormen ADHD</h1>
+        <p>Er bestaan drie typen ADHD:<br>
+
+            ADHD-I, vroeger ook wel ADD-type genoemd. Hierbij staat een aandachtstekort het meest op de voorgrond;<br>
+            <br>
+            ADHD-H, het overwegend hyperactieve en impulsieve type. Hier staan ernstige en aanhoudende impulsiviteit en hyperactiviteit op de voorgrond;<br>
+            <br>
+            ADHD-C, het gecombineerde type. Zowel problemen van het onoplettende als het hyperactieve type zijn aanwezig. Dit type ADHD komt het meeste voor en zal in deze tekst als uitgangspunt worden genomen.</p>
+            </div>
+            <div class="col-4">
+        <h1 id="home-titels">Symptomen ADHD</h1>
+        <p>Volwassenen met ADHD ervaren vooral problemen met organisatie, planning en concentratie. Daarnaast vertonen mensen met ADHD vaak ook hyperactief en impulsief gedrag. ADHD kenmerken bij volwassenen zijn over het algemeen hetzelfde als bij kinderen met ADHD.</p>
+    </div>
+    </div>
+    </div>
     </div>
 </div>
