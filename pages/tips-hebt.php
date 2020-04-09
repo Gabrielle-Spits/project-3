92cfb1cd6a308896515bb82e9663daaf08a6f8c5
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-04-09 15:32:30 +0200

    tips en informatie

--- pages/tips-hebt.php
+++ pages/tips-hebt.php
@@ -5,19 +5,36 @@
 </div>
 <div class="row">
     <div class="col-4">
+        <h3>prikkels</h3>
         <p>Zorg voor een opgeruimde kamer en bureau. Dit geeft minder prikkels.</p>
+        <h3>structuur & regelmaat</h3>
         <p>Zorg voor structuur en regelmaat. Denk aan een vaste dagindeling, of op vaste tijdstippen eten.</p>
-        <p>Voorkom afleiding. Zet de pop-ups van binnenkomende mails uit. Lees en beantwoord de mails op een vast moment op de dag.</p>
     </div>
     <div class="col-4">
+        <h3>afleiding</h3>
+        <p>Voorkom afleiding. Zet de pop-ups van binnenkomende mails uit. Lees en beantwoord de mails op een vast moment op de dag.</p>
+        <h3>deadlines</h3>
         <p>Werken met deadlines zijn een goede motivator en stimulans voor je. Ook dit helpt om je te concentreren. Dus ook grote werkzaamheden zijn geschikt voor je, als je die tenminste opdeelt in deelwerkzaamheden met ieder een eigen deadline.</p>
+    </div>
+    <div class="col-4">
+    <h3>pauze</h3>
         <p>Onderbreek je werk door bv koffie te halen. Vraag aan je docent of je even een rondje mag lopen. Zo kun je je energie kwijt.</p>
+    <h3>schrijven</h3>
         <p>Het kan helpen om 's avonds, voor het slapen gaan, de belangrijkste zaken die op dat moment door je hoofd heen schieten op te schrijven. Dat geeft rust en doordoor ook een betere nachtrust.</p>
     </div>
-    <div class="col-4">
+</div>
+<div class="row">
+    <div class="col-6">
+        <h3>plan</h3>
         <p>Plan activiteiten en werkzaamheden. Splits deze in kleine delen. Dat geeft overzicht en daarmee rust.</p>
-        <p>Ga regelmatig sporten. Geef jezelf de gelegenheid om je energie kwijt te raken en zicht uit te leven.</p>
+        <h3>planning</h3>
         <p>Maak een planning, eventueel met je leidinggevende, docenten, ouders of naaste collega’s.</p>
-        <p>Blijf met elkaar praten, verval niet in verwijten. Erger je je aan iets, zeg dat dan en zoek ook naar een oplossing.</p>
+    </div>
+    <div class="col-6">
+        <h3>sporten</h3>
+        <p>Ga regelmatig sporten. Geef jezelf de gelegenheid om je energie kwijt te raken en zicht uit te leven.</p>
+        <h3>blijven praten</h3>
+        <p>Blijf met elkaar praten, verval niet in verwijten. Erger je je aan iets, zeg dat dan en zoek ook naar een
+            oplossing.</p>
     </div>
 </div>
